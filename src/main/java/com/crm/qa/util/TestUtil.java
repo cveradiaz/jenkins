59c292bf0d3db353444a21848d430d91ed05e54a
cveradiaz <53835391+cveradiaz@users.noreply.github.com>
2020-06-29 23:35:02 -0400

    Update TestUtil.java

--- src/main/java/com/crm/qa/util/TestUtil.java
+++ src/main/java/com/crm/qa/util/TestUtil.java
@@ -21,7 +21,7 @@ public class TestUtil extends TestBase {
 	public static long PAGE_LOAD_TIMEOUT = 20;
 	public static long IMPLICIT_WAIT = 20;
 
-	public static String TESTDATA_SHEET_PATH = "C:\\Program Files (x86)\\Jenkins\\workspace\\prueba"
+	public static String TESTDATA_SHEET_PATH = System.getProperty("user.dir")
 			+ "/src/main/java/com/crm/qa/testdata/FreeCrmTestData.xlsx";
 
 	static Workbook book;
