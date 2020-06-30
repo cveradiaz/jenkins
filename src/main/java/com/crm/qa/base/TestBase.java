c553c3f06af95b1b4c7a9038d6911a702492f23a
cveradiaz <53835391+cveradiaz@users.noreply.github.com>
2020-06-29 23:22:24 -0400

    Update TestBase.java

--- src/main/java/com/crm/qa/base/TestBase.java
+++ src/main/java/com/crm/qa/base/TestBase.java
@@ -41,7 +41,7 @@ public class TestBase {
 		String browserName = prop.getProperty("browser");
 		
 		if(browserName.equals("chrome")){
-			System.setProperty("webdriver.chrome.driver", "C:\\drivers\\chromedriver.exe");
+			System.setProperty("webdriver.chrome.driver", System.getProperty("user.dir")+"/DriverNavegador");
 			driver = new ChromeDriver();
 			/*
 			ChromeOptions chromeOptions= new ChromeOptions();
